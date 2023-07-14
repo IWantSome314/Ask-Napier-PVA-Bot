6747e09495c664fbb3d1d431a8bf28298285ab05
IWantSome314 <85770767+IWantSome314@users.noreply.github.com>
2023-07-14 15:31:34 +0100

    Update Run_Scripts.py
    Changed file path for pipeline

--- Run_Scripts.py
+++ Run_Scripts.py
@@ -2,6 +2,6 @@
 #10/07/2023 - 10:44
 import subprocess
 #Add the file path below
-Path = "C:/Users/Hamish/Desktop/PVAAuto/Auto_Password_Reset.ps1"
+Path = "/home/GitHub/AutoPW/AutoPass"
 result = subprocess.run(["powershell", Path], capture_output=True, text=True)
 print(result.stdout)
