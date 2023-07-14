a1365505315604334d4da35eec6b8a27a357b0a4
IWantSome314 <85770767+IWantSome314@users.noreply.github.com>
2023-07-14 15:58:15 +0100

    Update Run_Scripts.py

--- Run_Scripts.py
+++ Run_Scripts.py
@@ -2,6 +2,6 @@
 #10/07/2023 - 10:44
 import subprocess
 #Add the file path below
-Path = "/usr/bin/bash --noprofile --norc /home/vsts/work/_temp/"
+Path = ""
 result = subprocess.run(["powershell", Path], capture_output=True, text=True)
 print(result.stdout)
