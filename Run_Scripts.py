c601c475b2a30a8c2cab7a301decfdd6fef5048a
IWantSome314 <85770767+IWantSome314@users.noreply.github.com>
2023-07-14 15:59:58 +0100

    Update Run_Scripts.py

--- Run_Scripts.py
+++ Run_Scripts.py
@@ -2,6 +2,6 @@
 #10/07/2023 - 10:44
 import subprocess
 #Add the file path below
-Path = ""
+Path = "/home/GitHub/AutoPW/AutoPass/Auto_Password_Reset.ps1"
 result = subprocess.run(["powershell", Path], capture_output=True, text=True)
 print(result.stdout)
