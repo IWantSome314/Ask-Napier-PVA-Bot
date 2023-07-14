4e9ed0a2669f182272f04019a45c5a7769cf2f86
IWantSome314 <85770767+IWantSome314@users.noreply.github.com>
2023-07-14 15:43:06 +0100

    Update Run_Scripts.py
    Test

--- Run_Scripts.py
+++ Run_Scripts.py
@@ -2,6 +2,6 @@
 #10/07/2023 - 10:44
 import subprocess
 #Add the file path below
-Path = "/home/GitHub/AutoPW/AutoPass"
+Path = "/usr/bin/bash --noprofile --norc /home/vsts/work/_temp/"
 result = subprocess.run(["powershell", Path], capture_output=True, text=True)
 print(result.stdout)
