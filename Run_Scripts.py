00d6ea4dff2ee3c24938a592f832606eb4cf6916
IWantSome314 <85770767+IWantSome314@users.noreply.github.com>
2023-07-24 09:46:55 +0100

    Update Run_Scripts.py
    Removed () on line 7

--- Run_Scripts.py
+++ Run_Scripts.py
@@ -4,5 +4,5 @@ import subprocess
 #Add the file path below
 Path = "/home/GitHub/AutoPW/AutoPass/Auto_Password_Reset.ps1"
 #result = (subprocess.run(["powershell", Path], capture_output=True, text=True))
-result = subprocess.call([Path])
+result = subprocess.call[Path]
 print(result.stdout)
